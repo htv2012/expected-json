e4a916de5f5064d1581bb43ae8169f5900b1608c
Hai Vu <haivu2004@gmail.com>
2024-09-08 18:25:52 -0700

    In working condition now

--- src/expected_json/expected_context.py
+++ src/expected_json/expected_context.py
@@ -7,8 +7,17 @@ class ExpectedJson:
         self.actual = actual
         self.errors = []
 
-    def key_value(self, path, value):
-        self.errors.append(f"{path=}, {value=}")
+    def key_value(self, path, expected):
+        actual = self.actual
+        try:
+            for key in path.split("."):
+                actual = actual[int(key)] if isinstance(actual, list) else actual[key]
+            if actual != expected:
+                self.errors.append(f"{path=}, {expected=}, {actual=}")
+        except KeyError:
+            self.errors.append(f"{path=}, {expected=}, key error: {key}")
+        except IndexError:
+            self.errors.append(f"{path=}, {expected=}, index error: {key}")
 
     def __enter__(self):
         self.errors = []
--- tests/test_context.py
+++ tests/test_context.py
@@ -3,21 +3,30 @@ import pytest
 from expected_json.expected_context import ExpectedJson
 
 
-def test_expect_pass():
-    with ExpectedJson({"name": "John"}) as expected:
-        assert expected is not None
+@pytest.fixture
+def actual():
+    return {"metadata": {"name": "env1", "tags": ["tag1", "tag2"]}}
+
+
+def test_expect_pass(actual):
+    with ExpectedJson(actual) as expected_json:
+        expected_json.key_value("metadata.name", "env1")
+        expected_json.key_value("metadata.tags.0", "tag1")
+        expected_json.key_value("metadata.tags.1", "tag2")
+
+
+# @pytest.mark.xfail
+def test_expect_fail(actual):
+    with ExpectedJson(actual) as expected_json:
+        expected_json.key_value("metadata.name", "env2")
+        expected_json.key_value("metadata.tags.0", "tag0")
+        expected_json.key_value("metadata.tags.9", "tag9")
+        expected_json.key_value("foo.bar", True)
 
 
 @pytest.mark.xfail
-def test_expect_fail():
-    with ExpectedJson({"name": "John"}) as expected:
-        expected.key_value("foo", "bar")
-        expected.key_value("x", "x-value")
-
-
-@pytest.mark.xfail
-def test_expect_error():
-    with ExpectedJson({"name": "John"}) as expected:
+def test_expect_error(actual):
+    with ExpectedJson(actual) as expected:
         expected.assert_value("foo", "bar")
         message = "Out of coffee"
         raise ValueError(message)
